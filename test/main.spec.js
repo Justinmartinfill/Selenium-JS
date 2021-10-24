ee1cefb6dd428ba330d2f74bd8cb1e4457ae7e0a
justinmartinfill <justinmartinfill@gmail.com>
2021-10-24 15:53:06 -0400

    setup complete and first webdriver tests

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,5 +1,36 @@
+const { Builder, By, Key, until } = require('selenium-webdriver')
+require('selenium-webdriver/chrome')
+require('selenium-webdriver/firefox')
+require('chromedriver')
+require('geckodriver')
+
 describe('test', () => {
-    it('does the thing', () => {
-        expect(1).toBe(1)
+    let driver
+    beforeAll((done) => {
+        new Builder().forBrowser('chrome').build().then((d) => {
+            driver = d 
+            done()
+        });
     })
+   
+    it('Confirms the Provi Website is up', async() => {
+    try {
+        await driver.get('https://www.provi.com/'); 
+        const title = await driver.getTitle()
+        
+        expect(title).toContain('Provi')              
+    }   finally {
+        await driver.quit();
+    }
+    })
+
+    test('the User can navigate to the sign in page', async() => {
+        // try {
+        // await driver.getCurrentUrl()
+        // } finally {
+        // await driver.quit();
+        // }      
+    })
+
+    test.todo('user can fill out the work email form and the contact form')
 })
