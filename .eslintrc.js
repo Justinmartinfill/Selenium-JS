b2b2770b28c2a212ca4543fc030e16a52670b300
justinmartinfill <justinmartinfill@gmail.com>
2021-10-24 14:48:27 -0400

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "jest/globals": true
+    },
+    "extends": [
+        "standard"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "rules": {
+    },
+    "plugins": [
+        "jest"
+    ]
+};
+
---
+++ test/main.spec.js
@@ -0,0 +1,5 @@
+describe('test', () => {
+    it('does the thing', () => {
+        expect(1).toBe(1)
+    })
+})
